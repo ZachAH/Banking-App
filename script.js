da438b09e7c1f7c57951df1220f4d1463eba4349
ZMAN <etta1055@gmail.com>
2021-07-24 11:28:45 -0500

    acc reduce method

--- script.js
+++ script.js
@@ -155,3 +155,11 @@ const calPrintBalance = function(movements) {
 // let balance2 = 0;
 // for (const mov of movements) balance2 += mov;
 // console.log(balance2);
+
+// Maximum value 
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov)
+  return acc;
+  else
+  return mov;
+}, movements[0])
